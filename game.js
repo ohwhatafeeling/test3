4d31019fe925fa8578836928c58556249c5f0ea4
Michael Macdonald <michaelmacdonald@Michaels-MacBook-Air.local>
2022-07-30 15:46:21 +1000

    Add fruit Items

--- game.js
+++ game.js
@@ -26,16 +26,98 @@ const game = new Phaser.Game(config);
 function preload() {
   this.load.image('tiles', 'assets/Terrain/Terrain (16x16).png');
   this.load.tilemapTiledJSON('map', 'assets/tilemaps/level3.json');
+  this.load.spritesheet('maskDudeRun', 'assets/Main Characters/Mask Dude/Run (32x32).png', {frameWidth: 32, frameHeight: 32});
+  this.load.spritesheet('maskDudeIdle', 'assets/Main Characters/Mask Dude/Idle (32x32).png', {frameWidth: 32, frameHeight: 32});
+  this.load.spritesheet('maskDudeJump', 'assets/Main Characters/Mask Dude/Jump (32x32).png', {frameWidth: 32, frameHeight: 32});
+  this.load.spritesheet('apple', 'assets/Items/Fruits/Apple.png', {frameWidth: 32, frameHeight: 32});
+  this.load.spritesheet('banana', 'assets/Items/Fruits/Bananas.png', {frameWidth: 32, frameHeight: 32});
+  this.load.spritesheet('cherry', 'assets/Items/Fruits/Cherries.png', {frameWidth:32, frameHeight: 32});
 }
 
 function create() {
   const map = this.make.tilemap({key: 'map'});
   const tileset = map.addTilesetImage('platformTiles', 'tiles');
-  const platform = map.createStaticLayer('Platform_Layer', tileset);
+  const platforms = map.createStaticLayer('Platform_Layer', tileset);
 
-  platform.setCollisionByExclusion(-1, true);
+  platforms.setCollisionByExclusion(-1, true);
+
+  this.apples = this.physics.add.group({
+    allowGravity: false,
+    immovable: true
+  });
+
+  this.bananas = this.physics.add.group({
+    allowGravity: false,
+    immovable: true
+  });
+
+  this.cherries = this.physics.add.group({
+    allowGravity: false,
+    immovable: true
+  });
+
+  map.getObjectLayer('Apples').objects.forEach(apple => {
+    const appleSprite = this.apples.create(apple.x, apple.y - 32, 'apple').setOrigin(0);
+  });
+
+  map.getObjectLayer('Bananas').objects.forEach(banana => {
+    const bananaSprite = this.bananas.create(banana.x, banana.y - 32, 'banana').setOrigin(0);
+  });
+
+  map.getObjectLayer('Cherries').objects.forEach(cherry => {
+    const cherrySprite = this.cherries.create(cherry.x, cherry.y - 32, 'cherry').setOrigin(0);
+  });
+
+  this.player = this.physics.add.sprite(176, 64, 'maskDudeIdle');
+  this.player.setBounce(0.1);
+  this.player.setCollideWorldBounds(true);
+  this.physics.add.collider(this.player, platforms);
+
+  this.anims.create({
+    key: 'run',
+    frames: this.anims.generateFrameNumbers('maskDudeRun', {start: 0, end: 11}),
+    frameRate: 20,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'idle',
+    frames: this.anims.generateFrameNumbers('maskDudeIdle', {start: 0, end: 10}),
+    frameRate: 20,
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'jump',
+    frames: this.anims.generateFrameNumbers('maskDudeJump', {start: 0, end: 0}),
+    frameRate: 20,
+    repeat: -1
+  });
+
+  this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
+
+  if (this.cursors.left.isDown) {
+    this.player.setVelocityX(-200);
+    if (this.player.body.onFloor) {
+      this.player.play('run', true);
+    }
+  } else if (this.cursors.right.isDown) {
+    this.player.setVelocityX(200);
+    if (this.player.body.onFloor) {
+      this.player.play('run', true);
+    }
+  } else {
+    this.player.setVelocityX(0);
+    if (this.player.body.onFloor) {
+      this.player.play('idle', true);
+    }
+  }
+  if ((this.cursors.up.isDown || this.cursors.space.isDown) && this.player.body.onFloor()) {
+    this.player.setVelocityY(-300);
+    this.player.play('jump', true);
+  }
 
 }
